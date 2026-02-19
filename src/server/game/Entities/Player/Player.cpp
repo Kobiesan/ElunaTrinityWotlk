532daa79020e5a6e53efaf4d4eceba3b926a5520
Kobiesan <tehwin@yahoo.com>
2026-02-18 19:31:36 -0600

    Add back spellInfo to Player.cpp

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -3469,7 +3469,15 @@ void Player::RemoveSpell(uint32 spell_id, bool disabled, bool learn_low_rank)
             SetUsedTalentCount(0);
     }
 
+    // update free primary prof.points (if not overflow setting, can be in case GM use before .learn prof. learning)
+    SpellInfo const* spellInfo = sSpellMgr->GetSpellInfo(spell_id);
     // Unlimited professions: profession points are no longer incremented on unlearn
+    //if (spellInfo && spellInfo->IsPrimaryProfessionFirstRank())
+    //{
+    //    uint32 freeProfs = GetFreePrimaryProfessionPoints() + 1;
+    //    if (freeProfs <= sWorld->getIntConfig(CONFIG_MAX_PRIMARY_TRADE_SKILL))
+    //        SetFreePrimaryProfessions(freeProfs);
+    //}
 
     // remove dependent skill
     SpellLearnSkillNode const* spellLearnSkill = sSpellMgr->GetSpellLearnSkill(spell_id);
